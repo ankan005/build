53796b1adbfa1b8d767ea603117e93b2244f6888
Jesse Chan <jc@lineageos.org>
2020-07-07 23:50:10 +0800

    merge
    Change-Id: I3d39616f3c8f7928f183f37e4b48333697b036af

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -151,6 +151,16 @@ class EdifyGenerator(object):
            ");")
     self.script.append(self.WordWrap(cmd))
 
+  def RunBackup(self, command, mount_point, dynamic=False):
+    if self.fstab:
+      p = self.fstab[mount_point]
+    if dynamic:
+      self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s", map_partition("%s"), "%s");' % (
+          command, p.device, p.fs_type)))
+    else:
+      self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s", "%s", "%s");' % (
+          command, p.device, p.fs_type)))
+
   def ShowProgress(self, frac, dur):
     """Update the progress bar, advancing it over 'frac' over the next
     'dur' seconds.  'dur' may be zero to advance it via SetProgress
